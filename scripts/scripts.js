4b502bae0987bf364ad17eed17607f83cb5bb727
wingsvn <wing-san.lee@live.nl>
2023-12-15 09:35:26 +0100

    nav + js v2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,19 +4,23 @@
 //dit gaat om het element met de classname .toggle
 
 const toggle = document.querySelector('.toggle');
-// console.log("button",toggle)
+//console.log("button",toggle)
 const nav = document.querySelector('.side-menu');
-// console.log("nav",nav)
+//console.log("nav",nav)
+const div = document.querySelector('.menu-toggle');
+//console.log(div)
 
 
 //hiermee zorg ik er voor dat na een 'click' event er een reactie zal plaatsvinden
-//toggle.addEventListener("click"), function() {}
 
 toggle.addEventListener('click', function(){
-    console.log("klik klik klik")
+    //console.log("klik klik klik")
 
     //na de 'click' event van de EventListener zal het volgende gebeuren:
-    nav.classList.toggle('active')
+    nav.classList.toggle('active'), //hiermee klapt de navigatiebar met links open
+    div.classList.toggle('active') //hiermee verschuift de toggle ook mee naar rechts
+    toggle.classList.toggle('active') //hiermee roteert de toggle 180deg, zodat het ook duidelijk is dat het weer inklapbaar is
+    
+    
     // nav.classList.toggle('side-menu.active') dit werkt niet,waarom?
-
 });
